44e598be1100b44818c33b101201e0725ea34171
Felipe Heitmann <felipeph@gmail.com>
2023-10-08 21:23:31 -0300

    yesterday

--- pages/last30days.py
+++ pages/last30days.py
@@ -32,8 +32,8 @@ timezone_brazil = pytz.timezone('America/Sao_Paulo')
 #params = st.experimental_get_query_params()
 #st.write(params)
 
-today = datetime.now(timezone_brazil)
-interval = timedelta(days=30)
+today = datetime.now(timezone_brazil) - timedelta(days=1)
+interval = timedelta(days=29)
 first_day = today - interval
 
 st.markdown("""
