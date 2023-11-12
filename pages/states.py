6ff82c4e4deb93c80f1534d328ad09bac9472fee
Felipe Heitmann <felipeph@gmail.com>
2023-11-12 16:41:16 -0300

    changes height

--- pages/states.py
+++ pages/states.py
@@ -117,4 +117,4 @@ if marked_states:
                      f'Number of Incidents: {df["estado"].value_counts()[state]}\n').add_to(info_state)
         info_state.add_to(map)
 
-day_map = st_folium(fig=map, height=700, width=500)
+st_folium(fig=map, width=200, height=300)
